5fac0f579428e10144376b39116209313efea32a
Edwin Marte Torres <kdgpro15@gmail.com>
2020-01-01 08:46:36 -0400

    010120001 - Adding code to turn off the UAC settings and removing the pause command

--- turn_down_network.ps1
+++ turn_down_network.ps1
@@ -1,9 +1,15 @@
 # As for this example, the $code variable saves the command to run as administrator
 
 $code = {
+    # To Remove the UAC //this script must be run with admin rights//
+    Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+
     Disable-NetAdapter E* -Confirm:$false   # Command to turn down the network
     Enable-NetAdapter E* -Confirm:$false    # Command to enable the newtwork
+
+}
+$turn_down_ethernet_network = {
+    Disable-NetAdapter E* -Confirm:$false
 }
 
-Start-Process -FilePath powershell.exe -ArgumentList $code -verb RunAs -WorkingDirectory C:     # Elevate an Admin Powershell to run the commands in the variable
-PAUSE
+Start-Process -FilePath powershell.exe -ArgumentList $turn_down_ethernet_network -verb RunAs -WorkingDirectory C:    # Elevate an Admin Powershell to run the commands in the variable
